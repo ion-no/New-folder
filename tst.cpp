4a229f62f150fdef0216fc66f94ee271b9f92244
Sriram K <sriram135175@gmail.com>
2025-01-02 20:41:04 +0530

    Test claude

---
+++ tst.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    stack<int> st;
+
+    st.push(1);
+    st.push(2);
+    st.push(3);
+
+    for(auto itr = st.begin(); itr != st.end();itr ++) {
+        cout<<itr.value;
+    }
+    return 0;
+}
+tst1
+tst2
+stsltj3
